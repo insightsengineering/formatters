fcec7174bd3627d825da3651ba2fe0de888d2128
shajoezhu <sha.joe.zhu@gmail.com>
2024-03-01 12:13:21 +0800

    update doc

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,7 @@ default_page_number <- function() {
 }
 
 #' Set default page number
+#' @param page_number (`string`)\cr Single string value to set the page number pattern
 #' @export
 set_default_page_number <- function(page_number) {
   options("formatter_default_page_number" = page_number)
