b013a7a27e85073a076b4d4e7f9130b1a8eebcea
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-06-10 16:16:24 +0200

    fix: add extra tests

--- tests/testthat/test-generics.R
+++ tests/testthat/test-generics.R
@@ -1,3 +1,11 @@
-testthat::test_that("nlines produces correct output with `max_width` defined", {
+testthat::test_that("nlines string is broken down in 2 lines with `max_width` defined", {
   testthat::expect_equal(nlines("1234567890", max_width = 5), 2)
+})
+
+testthat::test_that("nlines list argument is broken down in 3 lines with `max_width` defined", {
+  testthat::expect_equal(nlines(list("1234567890", "12345"), max_width = 5), 3)
+})
+
+testthat::test_that("nlines small string produces 1 line as output", {
+  testthat::expect_equal(nlines("1234567890"), 1)
 })
