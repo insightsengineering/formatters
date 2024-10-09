f9d9d70253aab5bd0e0e401d99f8e4ffac72d2df
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-10-09 14:30:23 -0400

    Clean up solution

--- R/matrix_form.R
+++ R/matrix_form.R
@@ -376,11 +376,17 @@ mf_update_cinfo <- function(mf, colwidths = NULL, rep_cols = NULL) {
     cinfo$self_extent <- r_colwidths
     nrepcols <- num_rep_cols(mf)
     rep_seq <- seq_len(nrepcols)
-    cinfo$par_extent <- if (!is.null(mf$listing_keycols) && length(r_colwidths) - nrepcols < 1) {
-      0
+
+    # empty listing
+    if (!is.null(mf$listing_keycols) && length(mf$listing_keycols) == 1 && length(r_colwidths) - nrepcols < 1) {
+      cinfo$par_extent <- 0
+    # listing with all key columns
+    } else if (!is.null(mf$listing_keycols) && mf_ncol(mf) == length(mf$listing_keycols)) {
+      cinfo$par_extent <- cumsum(c(0, cinfo$self_extent[seq_len(nrepcols - 1)]))
     } else {
-      cumsum(c(0, cinfo$self_extent[seq_len(nrepcols)], rep(0, length(r_colwidths) - nrepcols - 1)))
+      cinfo$par_extent <- cumsum(c(0, cinfo$self_extent[seq_len(nrepcols)], rep(0, length(r_colwidths) - nrepcols - 1)))
     }
+
     cinfo$reprint_inds <- I(lapply(seq_len(NROW(cinfo)), function(i) rep_seq[rep_seq < i]))
     mf_cinfo(mf) <- cinfo
   }
